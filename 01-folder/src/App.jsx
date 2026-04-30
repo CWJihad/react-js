f07880cd945dd140c64dbff0874daef45fda37b1
CWJihad <kmjihadhpl2023@gmail.com>
2026-04-30 11:57:40 +0600

    react introduction

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <>
+    <div id= "papa">
+      <h1 id='child1'>Hi</h1>
+      <h2 id='child2'>Hello</h2>
+      <h3 id='child3'>World!</h3>
+    </div>
+    
+    <div id="chacha"></div>
+    </>
+
+  )
+}
+
+export default App
