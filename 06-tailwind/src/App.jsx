9ddea5d1fb4b9743bb31ff871ffab47e7121eeb5
CWJihad <kmjihadhpl2023@gmail.com>
2026-05-03 16:42:17 +0600

    react with tailwind css

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='h-screen bg-gray-900 text-indigo-700 text-4xl flex items-center justify-center'>
+      React with tailwind css????
+    </div>
+  )
+}
+
+export default App
