c13847f6ceaf6607825b1bb128e39a906886c20b
CWJihad <kmjihadhpl2023@gmail.com>
2026-05-05 12:47:22 +0600

    Simple UI Project Complete

---
+++ 07-ui-project/src/components/Section2/Section2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section2 = () => {
+  return (
+    <div className='h-screen w-full border-8 border-amber-500'>
+      Section2
+    </div>
+  )
+}
+
+export default Section2
